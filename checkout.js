2e8fa26247a507d41d3d68b7cd02c5803ab0dc20
suryansh091 <tripathisuryansh091@gmail.com>
2026-03-31 14:26:52 +0530

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,1 +1,24 @@
 
+<script src="https://checkout.tebex.io/api/checkout.js"></script>
+
+<script>
+/* ✅ SINGLE ITEM BUY */
+function buyNow(id){
+  Tebex.checkout.open({
+    ident: id,
+    origin: window.location.origin
+  });
+}
+
+/* ✅ CART CHECKOUT */
+function checkoutCart(){
+  if(cart.length > 0){
+    Tebex.checkout.open({
+      ident: cart[0].id,   // buys first item (your current system)
+      origin: window.location.origin
+    });
+  } else {
+    alert("Cart is empty!");
+  }
+}
+</script>
